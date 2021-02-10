b6197cc6ed777cb004b166f2f35f9b04a0c697bd
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-10 03:00:12 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_concat(first,second)
+  first.concat(second)
+end
+
+def using_insert(array,blob) 
+  array.insert(4, blob)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array,string)
+  array.delete(string) 
+end 
+
+def using_delete_at(array,interger)
+  array.delete_at(interger)
+end
+
+
